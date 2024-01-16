ce03d8e4d9d1689b1f208f10c33d2fd263c0c2a7
PriChiafarelo <priscilachiafarelo@gmail.com>
2024-01-15 21:16:22 -0300

    primeiro commit

---
+++ desafio014.py
@@ -0,0 +1,4 @@
+"""Faço um conversor de temperatura de C para F"""
+
+temperatura = float(input('Digite a temperatura em C:'));
+print('A temperatura convertida para F é de {} F'.format(temperatura * 1.8 + 32))
